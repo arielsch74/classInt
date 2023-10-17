82d20467efbab6150a2c0aac47edfe5cf23112cb
arielsch74 <43975644+arielsch74@users.noreply.github.com>
2023-10-17 10:15:05 -0300

    Update angular-sample_test.js

--- angular-sample/angular-sample_test.js
+++ angular-sample/angular-sample_test.js
@@ -4,8 +4,8 @@ Scenario('Unificado', ({ I }) => {
     //I.amOnPage('http://localhost:4200');
 	I.amOnPage('https://gc-miang-z35laqzizq-uc.a.run.app');
 	I.click('Register');
-	I.amOnPage('/account/register')
-  I.fillField('[formcontrolname="firstName"]','emilia');
+  I.amOnPage('/account/register')
+  I.fillField('[formcontrolname="firstName"]','Emilia');
 	I.fillField('[formcontrolname="lastName"]','Schwindt');
 	I.fillField('[formcontrolname="username"]','emilia');
 	I.fillField('[formcontrolname="password"]','1234567890');
